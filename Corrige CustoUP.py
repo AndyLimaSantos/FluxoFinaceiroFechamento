a201640c6012fb58e978d70bb431892fe83a4d81
AndyLimaSantos <anderson210798@gmail.com>
2026-03-19 15:19:35 -0300

    Projeto financeiro Atualização 03

--- Corrige CustoUP.py
+++ Corrige CustoUP.py
@@ -5,32 +5,26 @@ presentes na primeira parte.
 '''
 import pandas as pd
 #Funções que iremos utilizar para concertar a planilha de Custo
-def insere_SKU(custoUP):
-    '''
-    Correção da coluna de SKU Armazem
-    a lógica é que para valores que o SKU
-    não existe mas exisyte o SKU normal
-    significa que ambos os valores são
-    iguais sendo necessário apenas uma
-    substituição do valor,
-    '''
+def insere_SKU(custoUP, dados_SKUArmazem):
     elementos_skuarmazem_nulo = list(custoUP[custoUP['SKU (Armazém)'].isnull()].index)
     for i in range(len(elementos_skuarmazem_nulo)):
-        custoUP.loc[elementos_skuarmazem_nulo[i],"SKU (Armazém)"] = custoUP.loc[elementos_skuarmazem_nulo[i],"SKU"]
+        sku_venda = custoUP.loc[elementos_skuarmazem_nulo[i],"SKU"]
+        indices = list(dados_SKUArmazem[dados_SKUArmazem["SKU Seller"] == sku_venda].index)
+        if len(indices) > 0:
+            custoUP.loc[elementos_skuarmazem_nulo[i],"SKU (Armazém)"]= dados_SKUArmazem.loc[indices[0],"SKU"]
+    #Modificação no custo
+    elementos_custoarmazem_nulo = list(custoUP[custoUP['Custo Médio'].isnull()].index)
+    for i in range(len(elementos_custoarmazem_nulo)):
+        sku_Armazem = custoUP.loc[elementos_custoarmazem_nulo[i],"SKU (Armazém)"]
+        sku_venda = custoUP.loc[elementos_custoarmazem_nulo[i],"SKU"]
+        indices_armazem = list(dados_SKUArmazem[dados_SKUArmazem["SKU"] == sku_Armazem].index)
+        indices_venda = list(dados_SKUArmazem[dados_SKUArmazem["SKU Seller"] == sku_venda].index)
+        condicional = True
+        if len(indices_armazem) > 0 and condicional == True:
+            custoUP.loc[elementos_custoarmazem_nulo[i],"Custo Médio"]= dados_SKUArmazem.loc[indices_armazem[0],"Custo Médio"]
+        if len(indices_venda) > 0 and condicional == True:
+            custoUP.loc[elementos_custoarmazem_nulo[i],"Custo Médio"]= dados_SKUArmazem.loc[indices_venda[0],"Custo Médio"]
     return custoUP
-
-def insere_custo(custoUP, dataSKU):
-    elementos_custo_nulo = list(custoUP[custoUP['Custo Médio'].isnull()].index) #faz uma lista de todos os indices que contem custo nulo
-    for i in range(len(elementos_custo_nulo)):
-        SKU_armazem = custoUP.loc[elementos_custo_nulo[i],"SKU (Armazém)"] #VALOR DO SKU DO ARMAZEM.
-        indice = dataSKU.loc[dataSKU['SKU'] == SKU_armazem].index #retorna o index do valor do sku encontrado  na planilha de sku2skuarmazem)
-        if not indice.empty:
-            valor = int(indice[0])
-            custoUP.loc[elementos_custo_nulo[i],"Custo Médio"] = dados_SKUArmazem.loc[valor,'Custo Médio']
-        else:
-            custoUP.loc[elementos_custo_nulo[i],"Custo Médio"] = 0
-    return custoUP
-
 
 def main():
     #Exportamos os bancos de dados que queremos trabalhar 
@@ -38,15 +32,10 @@ def main():
     dados_custoUP = pd.read_csv("custoUP.csv").drop("Unnamed: 0", axis =1)
     dados_todosPedidos = pd.read_csv("todosPedidos.csv").drop("Unnamed: 0", axis =1)
     dados_devolucoes = pd.read_csv("devolucoes.csv").drop("Unnamed: 0", axis =1)
-    dados_SKUArmazem = pd.read_csv("SKU2SKUarmazem.csv")
+    dados_SKUArmazem = pd.read_csv("SKU2SKUarmazem.csv").drop("Unnamed: 0", axis =1)
 
-    #primeira correção da planilha, inserimos os valores de SKU do Armazem que faltam.
-
-    # Correção dos valores de SKU's do Armazém
-    dados_custoUP_filtro = insere_SKU(dados_custoUP)
-    # Correção dos valores de custo
-    custoUP_data = insere_custo(dados_custoUP_filtro, dados_SKUArmazem)
-
+    #Correção e inserção dos valores esperados
+    dados = insere_SKU(dados_custoUP, dados_SKUArmazem)
     #Salvar a planilha:
     custoUP_data.to_excel("custoUP.xlsx")
     custoUP_data.to_csv("custoUP.csv")
