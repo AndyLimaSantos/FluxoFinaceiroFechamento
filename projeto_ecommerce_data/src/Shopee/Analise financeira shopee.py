e4fb471a5ef98729b1637f1c1433da3d319ea7ff
AndyLimaSantos <anderson210798@gmail.com>
2026-04-01 15:46:50 -0300

    Atualização do sxcript da shopee 02

--- projeto_ecommerce_data/src/Shopee/Analise financeira shopee.py
+++ projeto_ecommerce_data/src/Shopee/Analise financeira shopee.py
@@ -1,3 +1,110 @@
-#Importações das blibliotecas que utilizaremos.
-#Funções que usaremos durante o processo.
-#Importação dos dados que serão analisados.
+#Importacoes das blibliotecas que utilizaremos.
+import pandas as pd
+#Funcoes utilizadas
+def banco_sem_duplas(dados):
+    #cria um banco sem as duplicacoes
+    return dados[dados.duplicated(subset=['ID do pedido'], keep=False) == False]
+def banco_duplicado(dados):
+    #cria um banco apenas de duplicacoes
+    return dados[dados.duplicated(subset=['ID do pedido'], keep=False)]
+def primeiros_duplicado(dados):
+    #cria um banco com apenas os primeiros elementos das duplicacoes
+    #utilizam o dataframe duplicado
+    return dados[dados.duplicated(subset=['ID do pedido'], keep='first') == False]
+def dados_para_analise(dados):
+    #gera o banco que ira ser analisado return = 0
+    bancos = {bancos: grupo for bancos, grupo in dados.groupby('Returned quantity')}
+    return bancos[0]
+def receita(dados):
+    #gera a receita
+    return dados['Subtotal do produto'].sum()
+def frete(dados):
+    #gera o frete
+    valorEstimado = dados['Valor estimado do frete'].sum()
+    taxaPagaCom = dados['Taxa de envio pagas pelo comprador'].sum()
+    desconto = dados['Desconto de Frete Aproximado'].sum()
+    return valorEstimado - taxaPagaCom - desconto
+def taxa_servico(dados):
+    #gera a taxa de servico
+    return dados['Taxa de serviço bruta'].sum()
+def taxa_transacao(dados):
+    #gera a taxa de transacao
+    return dados['Taxa de transação'].sum()   
+def comissao_shopee(dados):
+    return dados['Taxa de comissão bruta'].sum()
+def custo(dados):
+    #gera o valor do custo
+    return dados['Custo total'].sum()
+def insere_custo(dadosP, dadosCusto):
+    '''utiliza dos dados dos pedidos e procura entre os valores do custo o custo
+    daquele produto pedido'''
+    '''(dados de pedidos, dados de custo) -> um Series com os valores de cada um dos 
+    custos identificados através da relação de nome de anuncio'''
+    linhas = dadosP.shape[0]
+    custo = []
+    for i in range(0,linhas):
+        nome_produto = dadosP.loc[i,'Nome do Produto']
+        indices = list(dadosCusto[dadosCusto['Nome do Anúncio'] == nome_produto].index)
+        if len(indices) == 0:
+            custo.append(0)
+        if len(indices) > 0:
+            custo.append(dadosCusto.loc[indices[0],'Custo Médio'])
+    return custo
+#_______________main____________________
+def main():
+    # Nome dos bancos de dados, precisamos de quatro bancos
+    # 1° dados dos pedidos feitos no ultimo mês
+    # 2° dados dos custos que tivemos no mês
+    # 3° dados de SKU, a relação existentes
+    # 4° dados de ID considerados anteriormente
+    nome_pedidos = str(input())
+    nome_custo = str(input())
+    nomeSKU = str(input())
+    nome_ID = str(input())
+    #dados de pedidos
+    dadosDePedidos =  pd.read_excel(f"{nome_pedidos}.xlsx")
+    #dados de custo
+    dadosDeCusto = pd.read_excel(f"{nome_custo}.xlsx")
+    #dados de SKU2SKU Armazem
+    dadosSKU2SKUA = pd.read_excel(f"{nomeSKU}.xlsx")
+    #dados de ID Considerados
+    dadosIdConsiderados = pd.read_excel(f"{nome_ID}.xlsx") 
+    #filtro das colunas que desejamos
+    dadosFiltrados = pd.DataFrame(data = {'ID do pedido':dadosDePedidos['ID do pedido'].astype(str),\
+                                          'Status do pedido':dadosDePedidos['Status do pedido'].astype(str),\
+                                          'Nome do Produto':dadosDePedidos['Nome do Produto'].astype(str),\
+                                          'Número de referência SKU':dadosDePedidos['Número de referência SKU'],\
+                                          'Returned quantity':dadosDePedidos['Returned quantity'].astype(int),\
+                                          'Subtotal do produto':dadosDePedidos['Subtotal do produto'].astype(float),\
+                                          'Preço original':dadosDePedidos['Preço original'].astype(float),\
+                                          'Desconto do vendedor':dadosDePedidos['Desconto do vendedor'].astype(float),\
+                                          'Quantidade':dadosDePedidos['Quantidade'],\
+                                          'Valor estimado do frete':dadosDePedidos['Valor estimado do frete'].astype(float),\
+                                          'Taxa de envio pagas pelo comprador':dadosDePedidos['Taxa de envio pagas pelo comprador'].astype(float),\
+                                          'Desconto de Frete Aproximado':dadosDePedidos['Desconto de Frete Aproximado'].astype(float),\
+                                          'Taxa de transação':dadosDePedidos['Taxa de transação'].astype(float),\
+                                          'Taxa de Envio Reversa':dadosDePedidos['Taxa de Envio Reversa'].astype(float),\
+                                          'Taxa de comissão bruta':dadosDePedidos['Taxa de comissão bruta'].astype(float),\
+                                          'Taxa de serviço bruta':dadosDePedidos['Taxa de serviço bruta'].astype(float)})
+    
+    dadosFiltrados_custo = pd.DataFrame(data = {'SKU':dadosDeCusto['SKU'].astype(str),\
+                                                'Nome do Anúncio':dadosDeCusto['Nome do Anúncio'].astype(str),\
+                                                'SKU (Armazém)':dadosDeCusto['SKU (Armazém)'].astype(str),\
+                                                'Custo Médio':dadosDeCusto['Custo Médio'].astype(float)})
+    #inserção dos custos no conjunto de dados de pedidos
+    custos = insere_custo(dadosFiltrados, dadosFiltrados_custo)
+    dadosFiltrados["Custo Unitário"] = custos
+    dadosFiltrados["Custo total"] = dadosFiltrados['Quantidade'] * dadosFiltrados["Custo Unitário"]
+    #filtramos os pedidos que estão cancelados.
+    dadosFiltrados = dadosFiltrados[dadosFiltrados['Status do pedido'] != 'Cancelado']
+    #resetamos os indices para possiveis interações
+    dadosFiltrados_reset = dadosFiltrados.reset_index()
+    #retiramos a coluna com os indices anteriores
+    dadosFiltradosNovos  = dadosFiltrados_reset.drop("index", axis = 1)
+    #____________________________________Bloco calculo dos valores copnsiderados na ultima finalizacao______________________________________________
+    
+    #____________________________________Bloco para calculo dos valores que estão mais atuais_______________________________________________________
+    
+    #____________________________________Escrita dos arquivos de verificação externa________________________________________________________________
+if __name__ == "__main__":
+    main()
